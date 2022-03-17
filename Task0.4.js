b29fab3a13ddd400ab53e69122478b1188573590
unknown <keanesmall99@gmail.com>
2022-03-17 18:39:57 +0200

    Task 0.4 Completed

---
+++ Task0.4.js
@@ -0,0 +1,11 @@
+function evenOrOdd(integer){
+    if (integer % 2 == 0){     // if a is number divided by 2 and has a remainer it's odd
+        console.log("even")
+    }else {
+        console.log("odd");
+    }
+
+}
+
+evenOrOdd(3);
+evenOrOdd(4);
