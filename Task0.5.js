2ba93bde66845f14cdb812ce75881b38dd959f1e
unknown <keanesmall99@gmail.com>
2022-03-17 18:53:12 +0200

    Added a comment on Task 0.5.js

--- Task0.5.js
+++ Task0.5.js
@@ -1,6 +1,6 @@
 function areaOfTriangle(sOne,sTwo,sThree){
     let semiPerimeter = (0.5)*(sOne + sTwo + sThree);
-    let area = Math.sqrt(semiPerimeter*((semiPerimeter - sOne)*(semiPerimeter - sTwo)*(semiPerimeter - sThree)));
+    let area = Math.sqrt(semiPerimeter*((semiPerimeter - sOne)*(semiPerimeter - sTwo)*(semiPerimeter - sThree))); // formula to find area
     return console.log(area);
 }
 
