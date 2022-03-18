f3a83fe74d8c90ea8db3f1ba8352da5d69d19269
unknown <keanesmall99@gmail.com>
2022-03-18 11:32:17 +0200

    Task 0.9 completed

---
+++ Task0.9.js
@@ -0,0 +1,23 @@
+function findVowels (word){
+    word.toLowerCase();
+    let i = 0;
+    let vowels = "";
+    if (word.indexOf("a") !== -1){          // Used .indexOf to find the letter
+        vowels += " a,";
+    }
+    if (word.indexOf("e") !== -1){
+        vowels += " e,";
+    }
+    if (word.indexOf("i") !== -1){
+        vowels += " i,";
+    }
+    if (word.indexOf("o") !== -1){
+        vowels += " o,";
+    }
+    if (word.indexOf("u") !== -1){
+        vowels += " u,";
+    }
+    return console.log("Vowels:" + vowels.slice(0,-1)); // used .slice to remove the last comma.
+}
+
+findVowels("Uzumi");
