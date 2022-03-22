69c0555a6cf8e835cded99d4b75e1879ede5eb82
unknown <keanesmall99@gmail.com>
2022-03-22 15:32:02 +0200

    Edited Task0.8.js

--- Task0.8.js
+++ Task0.8.js
@@ -1,17 +1,19 @@
-function hoursAndMinutes(num){
-    const numHours = Math.floor(num/60);
-    const numMinutes = num % 60;
-    
-    if (numHours == 1 && numMinutes == 1){
-        console.log(numHours + " hour, " + numMinutes + " minute ");
-    }else if (numHours > 1 && numMinutes > 1){
-        console.log(numHours + " hours, " + numMinutes + " minutes");
-    }else if (numHours > 1 && numMinutes == 1 ){
-        console.log(numHours + " hours, " + numMinutes + " minute");
-    }else {
-        console.log(numHours + " hour, " + numMinutes + " minutes");
-    }
+function hoursAndMinutes(num) {
+  const numHours = Math.floor(num / 60);
+  const numMinutes = num % 60;
+
+  if (numHours == 1 && numMinutes == 1) {
+    console.log(numHours + " hour, " + numMinutes + " minute ");
+  } else if (numHours > 1 && numMinutes > 1) {
+    console.log(numHours + " hours, " + numMinutes + " minutes");
+  } else if (numHours > 1 && numMinutes == 1) {
+    console.log(numHours + " hours, " + numMinutes + " minute");
+  } else if (numHours > 1 && numMinutes < 1 ){
+    console.log(numHours + " hours, " + numMinutes + " minutes");
+  }else {
+    console.log(numHours + " hour, " + numMinutes + " minutes");
+  }
 }
 
-hoursAndMinutes(71);
-hoursAndMinutes(121);
+hoursAndMinutes(180);
+hoursAndMinutes(120);
